d5c04a32ffb3535fdf1777d09dbbd7ac567161f6
edpyt <edrsa1321s@gmail.com>
2024-01-17 20:26:55 +0300

    add alembic

--- src/infrastructure/db/models/__init__.py
+++ src/infrastructure/db/models/__init__.py
@@ -1,2 +1,3 @@
-from order import Order  # noqa: F401
-from product import Product  # noqa: F401
+from src.infrastructure.db.models.base import BaseModel  # noqa: F401
+from src.infrastructure.db.models.order import Order  # noqa: F401
+from src.infrastructure.db.models.product import Product  # noqa: F401
