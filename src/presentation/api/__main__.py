d0c387af3b5340d2e1d4933d8906eec6588af571
edpyt <edrsa1321s@gmail.com>
2024-02-05 22:34:44 +0300

    refactor: update start application

---
+++ src/presentation/api/__main__.py
@@ -0,0 +1,12 @@
+from src.presentation.api.main import build_app, run_app
+
+
+async def main() -> None:
+    app = build_app()
+    await run_app(app)
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(main())
--- src/presentation/api/main.py
+++ src/presentation/api/main.py
@@ -1,4 +1,7 @@
+import logging
+
 from litestar import Litestar
+import uvicorn
 
 from src.infrastructure.di.main import setup_di
 from src.presentation.api.routes import setup_routes
@@ -11,3 +14,15 @@ def build_app() -> Litestar:
     setup_routes(app)
 
     return app
+
+
+async def run_app(app: Litestar) -> None:
+    config = uvicorn.Config(
+        app=app,
+        host="0.0.0.0",
+        port=8000,
+        log_level=logging.INFO,
+        log_config=None,
+    )
+    server = uvicorn.Server(config)
+    await server.serve()
