40d5997dcbf2a906ad28a233e41446cd8ca45cf4
edpyt <edrsa1321s@gmail.com>
2024-02-05 21:27:46 +0300

    refactor: update `get_products` dto

--- src/infrastructure/db/repositories/product.py
+++ src/infrastructure/db/repositories/product.py
@@ -10,4 +10,13 @@ class ProductReaderImpl(SQLAlchemyRepo, ProductReader):
     async def get_products(self) -> list[dto.ProductDTO]:
         stmt = select(Product)
         result = await self._session.scalars(stmt)
-        return list(map(dto.ProductDTO, result))
+        return list(
+            map(
+                lambda product: dto.ProductDTO(
+                    title=product.title,
+                    price=product.price,
+                    weight=product.weight,
+                ),
+                result.all(),
+            )
+        )
