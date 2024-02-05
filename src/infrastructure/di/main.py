965facf765a415c7f5217e358fa47eccf0882fb8
edpyt <edrsa1321s@gmail.com>
2024-02-05 21:27:25 +0300

    chor:e: setup di

--- src/infrastructure/di/main.py
+++ src/infrastructure/di/main.py
@@ -1,5 +1,11 @@
 from litestar import Litestar
+from litestar.di import Provide
+
+from src.application.common.config.parser.main import load_config
+from src.infrastructure.db.main import build_async_engine, db_async_session
 
 
 def setup_di(app: Litestar) -> None:  # noqa: ARG001
-    ...
+    app.dependencies["db_config"] = lambda: load_config(type_config="db")
+    app.dependencies["engine"] = Provide(build_async_engine)
+    app.dependencies["session"] = Provide(db_async_session)
--- src/presentation/api/main.py
+++ src/presentation/api/main.py
@@ -1,11 +1,13 @@
 from litestar import Litestar
 
+from src.infrastructure.di.main import setup_di
 from src.presentation.api.routes import setup_routes
 
 
 def build_app() -> Litestar:
     app = Litestar()
 
+    setup_di(app)
     setup_routes(app)
 
     return app
