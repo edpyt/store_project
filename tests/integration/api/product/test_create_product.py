57e78dc341415f1e731a919b6cb32aa8bbf4d625
edpyt <edrsa1321s@gmail.com>
2024-02-07 22:34:13 +0300

    test: add wrong post route tests

--- tests/integration/api/product/test_create_product.py
+++ tests/integration/api/product/test_create_product.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pytest
 from httpx import AsyncClient
 
@@ -11,3 +12,15 @@ async def test_create_product(client: AsyncClient):
 
     assert response.status_code == 201
     assert response.json() == {"message": "Created product!"}
+
+
+@pytest.mark.parametrize("create_product_data", [
+    {"tsti": "dsaj", "dsajdjsa": .1, "erd": "E"},
+    {"dsad": .1},
+    [1, 2, 3, 4]
+])
+@pytest.mark.asyncio
+async def test_wrong_create_product(client: AsyncClient, create_product_data: Any):
+    response = await client.post("/product/create/", data=create_product_data)
+
+    assert response.status_code == 400
