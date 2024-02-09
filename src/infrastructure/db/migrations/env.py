4ced62b679e6e8b1cb1df7e4ce8e195d829eca01
edpyt <edrsa1321s@gmail.com>
2024-02-09 20:46:08 +0300

    refactor: update `alembic` configuration

--- src/infrastructure/db/migrations/env.py
+++ src/infrastructure/db/migrations/env.py
@@ -1,15 +1,14 @@
 import asyncio
-from logging.config import fileConfig
 import os
+from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from alembic import context
-from src.presentation.api.config.parser import load_config
-
 from src.infrastructure.db.models import BaseModel
+from src.presentation.api.config.parser import load_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
