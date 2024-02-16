bf647b3ba0ff33a350d80ac2b31f1d49993d718b
edpyt <edrsa1321s@gmail.com>
2024-02-16 17:49:48 +0300

    refactor: remove transactional tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,49 +38,39 @@ def db_config(path: str) -> DBConfig:
     return load_config(path, "db")
 
 
-@pytest_asyncio.fixture(name="engine", scope="session")
+@pytest_asyncio.fixture(name="engine")
 async def create_engine(db_config: DBConfig) -> AsyncGenerator[AsyncEngine, None]:
     engine = await anext(build_async_engine(db_config))
     yield engine
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture
 async def tables(engine: AsyncEngine) -> AsyncGenerator[None, None]:
-    async with engine.connect() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(BaseModel.metadata.create_all)
-        await conn.commit()
-        try:
-            yield conn
-        finally:
-            await conn.run_sync(BaseModel.metadata.drop_all)
+    yield conn
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all)
 
 
 @pytest_asyncio.fixture(scope="function")
 async def session(engine: AsyncEngine, tables: None) -> AsyncGenerator[AsyncSession, None]:
-    async with engine.connect() as conn:
-        transaction = await conn.begin()
-        sessionmaker = async_sessionmaker(
-            bind=conn,
-            autoflush=False,
-            expire_on_commit=False,
-            autocommit=False,
-            join_transaction_mode="create_savepoint",
-        )
-        async with sessionmaker() as session:
-            yield session
-        await transaction.rollback()
+    sessionmaker = async_sessionmaker(
+        bind=engine,
+        autoflush=False,
+        expire_on_commit=False,
+        autocommit=False,
+    )
+    async with sessionmaker() as session:
+        yield session
 
 
 @pytest_asyncio.fixture
-async def created_product(engine: AsyncEngine) -> Product:
-    async with AsyncSession(engine) as session:
-        product = Product(title="milk", price=0.5, weight=1000)
+async def created_product(session: AsyncSession) -> Product:
+    product = Product(title="milk", price=0.5, weight=1000)
 
-        session.add(product)
-        await session.commit()
-        await session.refresh(product)
+    session.add(product)
+    await session.commit()
+    await session.refresh(product)
 
-        yield product
-
-        session.delete(product)
-        await session.commit()
+    yield product
